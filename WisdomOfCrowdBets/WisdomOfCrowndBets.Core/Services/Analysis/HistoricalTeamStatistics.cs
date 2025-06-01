c896cab3531408b7dc86d6a13a878e6b598e915d
leainfo22 <leandro.vasquez.mora@gmail.com>
2025-06-01 20:03:25 +1000

    data analysis fixed

--- WisdomOfCrowdBets/WisdomOfCrowndBets.Core/Services/Analysis/HistoricalTeamStatistics.cs
+++ WisdomOfCrowdBets/WisdomOfCrowndBets.Core/Services/Analysis/HistoricalTeamStatistics.cs
@@ -17,13 +17,11 @@ namespace WisdomOfCrowndBets.Core.Services.Analysis
             List<TeamStatistic> teamStatistics = new List<TeamStatistic>();
             try
             {
-                
                 var teamStats = listHistoricalDataXlsx
                     .SelectMany(x => new[]
                     {
-                        new { Team = x.HomeTeam, IsHome = true, IsWinner = x.Winner == "Home", },
-                        new { Team = x.AwayTeam, IsHome = false, IsWinner = x.Winner == "Away" }
-                       
+                        new { Team = x.HomeTeam, IsHome = true, IsWinner = x.Winner == x.HomeTeam },
+                        new { Team = x.AwayTeam, IsHome = false, IsWinner = x.Winner == x.AwayTeam }
                     })
                     .GroupBy(x => x.Team)
                     .Select(g => new
@@ -36,30 +34,14 @@ namespace WisdomOfCrowndBets.Core.Services.Analysis
 
                 foreach (var stat in teamStats)
                 {
-                    teamStatistics.Add(new TeamStatistic
-                    {
-                        team_name = stat.Team,
-                        matches_played = stat.HomeWins + stat.AwayWins,
-                        wins = stat.HomeWins + stat.AwayWins,
-                        losses = ,
-                        home_wins = stat.HomeWins,
-                        away_wins = stat.AwayWins
-
-                        /*  string? team_name { get; set; }
-        int matches_played { get; set; }
-        int wins { get; set; }
-        int losses { get; set; }
-        int home_wins { get; set; }
-        int away_wins { get; set; }*/
-                    })
                     Console.WriteLine($"Team: {stat.Team}, Home Wins: {stat.HomeWins}, Away Wins: {stat.AwayWins}");
                 }
-                
                 return teamStatistics;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                return teamStatistics;
             }
         }
     }
